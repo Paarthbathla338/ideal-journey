31aade41b26d94886793b1ca60896000b168bdba
Paarthbathla338 <Paarth.bathla@outlook.com>
2022-02-15 20:32:21 +0530

    Create Doc Complete

--- firestortut/App.js
+++ firestortut/App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import {db} from "./Core/Config.js"
+import {doc, setDoc} from "firebase/firestore"
+
 
 export default function App() {
 
+
   const Create=()=>{
+    const myDoc=doc(db,"MyCollection","MyDoc")
+    const docData={
+      name:"Paarth",
+      age:14,
+      residence:"newDelhi"
+    }
+    setDoc(myDoc,docData)
+    .then(()=>{
+      alert("Document Created")
+    })
+    .catch((error)=>{alert(error.message)})
 
   }
   const Read=()=>{
@@ -26,7 +40,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-    <Button title="Create New Doc"></Button>
+    <Button title="Create New Doc" onPress={Create}></Button>
     </View>
   );
 }
